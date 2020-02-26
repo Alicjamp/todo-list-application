e4eab95cbee1a19399157bbdb6c41fc49224e009
Alicjamp <alicja.parker@outlook.com>
2020-02-26 17:12:10 +0000

    alert added on empty todo input

--- src/components/Form.js
+++ src/components/Form.js
@@ -37,12 +37,16 @@ class Form extends React.Component {
 
     addTodo = (event) => {
         console.log(this.state)
+        if (this.state.task === "") {
+            alert("Error! Input task in Todo field")
+        } else {
 
-        this.props.addTodoFunc(
-            this.state.task,
-            this.state.dueDate,
-            this.state.important
-        )
+            this.props.addTodoFunc(
+                this.state.task,
+                this.state.dueDate,
+                this.state.important
+            )
+        }
 
         this.setState({
             task: "",
@@ -50,38 +54,38 @@ class Form extends React.Component {
             dueDate: moment().format("YYYY-MM-DD"),
         })
 
-    event.preventDefault();
-}
+        event.preventDefault();
+    }
 
-render() {
-    return (
-        <div>
-            <form>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-md-4">
-                            <label htmlFor="Todo">Todo</label><br></br>
-                            <input id="Todo" type="text" onChange={this.updateTask} value={this.state.task} placeholder="E.g. Take over the world" /><br></br>
-                        </div>
-                        <div className="col-12 col-md-3">
-                            <label htmlFor="Due by date">Due by date</label><br></br>
-                            <input type="date" onChange={this.updateDueDate} value={this.state.dueDate} /><br></br>
-                        </div>
-                        <div className="col-12 col-md-3">
-                            <label htmlFor="Priority">Priority</label><br></br>
-                            <label htmlFor="Low"><input onClick={this.setLowPriority} id="Low" type="radio" name="Low-High" value="Low" selected /> Low</label>
-                            <label htmlFor="High"><input onClick={this.setHighPriority} id="High" type="radio" name="Low-High" value="High" /> High</label>
-                        </div>
-                        <div className="col-12 col-md-1">
-                            <button type="submit" className="add" onClick={this.addTodo} >Add</button>
+    render() {
+        return (
+            <div>
+                <form>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12 col-md-4">
+                                <label htmlFor="Todo">Todo</label><br></br>
+                                <input id="Todo" type="text" onChange={this.updateTask} value={this.state.task} placeholder="E.g. Take over the world" /><br></br>
+                            </div>
+                            <div className="col-12 col-md-3">
+                                <label htmlFor="Due by date">Due by date</label><br></br>
+                                <input type="date" onChange={this.updateDueDate} value={this.state.dueDate} /><br></br>
+                            </div>
+                            <div className="col-12 col-md-3">
+                                <label htmlFor="Priority">Priority</label><br></br>
+                                <label htmlFor="Low"><input onClick={this.setLowPriority} id="Low" type="radio" name="Low-High" value="Low" selected /> Low</label>
+                                <label htmlFor="High"><input onClick={this.setHighPriority} id="High" type="radio" name="Low-High" value="High" /> High</label>
+                            </div>
+                            <div className="col-12 col-md-1">
+                                <button type="submit" className="add" onClick={this.addTodo} >Add</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </form>
-        </div>
+                </form>
+            </div>
 
-    )
-}
+        )
+    }
 }
 
 export default Form;
